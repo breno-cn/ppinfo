58ebf8493540b33a4bca8df31a27d5ed44a0c2f1
breno-cn <breno.cn@hotmail.com>
2021-10-04 19:45:49 -0300

    init

---
+++ src/main/java/com/backend/ppinfo/PpinfoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ppinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/ppinfo/PpinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ppinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
